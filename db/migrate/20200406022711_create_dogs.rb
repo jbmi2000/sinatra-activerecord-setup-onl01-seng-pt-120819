133ed709feafb1a50149000a04870e192f1a2fba
Jeffrey Marbury <jeff.marbury@gmail.com>
2020-04-06 02:33:14 +0000

    Automatically backed up by Learn

--- db/migrate/20200406022711_create_dogs.rb
+++ db/migrate/20200406022711_create_dogs.rb
@@ -2,10 +2,10 @@ class CreateDogs < ActiveRecord::Migration[5.2]
 
   def up
     create_table :dogs do |t|
-    t.string :name
-    t.string :breed
-  end
-
+      t.string :name
+      t.string :breed
+    end
+  
   end
 
   def down
